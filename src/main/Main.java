c56d6b1a962fa06b03b872e6b915536dc97f0c05
daniil_panov <daniil_panov2005@mail.ru>
2018-12-04 12:50:06 +0300

    First commit (create template)

---
+++ src/main/Main.java
@@ -0,0 +1,42 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+
+class Main
+{
+    public static JFrame frame = new JFrame("TestGame");
+    
+    private Main()
+    {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        frame.add(new Menu());
+        
+        frame.setVisible(true);
+        frame.setExtendedState(Frame.MAXIMIZED_BOTH);
+    }
+    
+    public static void main(String[] args)
+    {
+        try
+        {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels())
+            {
+                if ("Nimbus".equals(info.getName()))
+                {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        }
+        catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+                | UnsupportedLookAndFeelException e
+        )
+        {
+            e.printStackTrace();
+        }
+        //
+        new Main();
+    }
+}
---
+++ src/main/Menu.java
@@ -0,0 +1,13 @@
+package main;
+
+import javax.swing.*;
+
+public class Menu extends JPanel
+{
+    private JFrame frame = Main.frame;
+    
+    public Menu()
+    {
+    
+    }
+}
